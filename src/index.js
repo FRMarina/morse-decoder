345665544cbc04fbd3f67e8928473f043d86d528
FRMarina <marinafedchik@gmail.com>
2020-10-24 23:23:48 +0300

    init: solve a problem

--- src/index.js
+++ src/index.js
@@ -38,7 +38,46 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = expr.match(/(.{1,10})/gim),
+    newArr = [];
+arr.forEach(function(e){
+  let regArr = e.match(/(.{1,2})/gim);
+  newArr.push(regArr);
+  return newArr;
+});
+
+for(let item of newArr){
+  for(let i = 0; i < item.length; i++){
+    if(item[i] == "10"){
+      item.splice(i,1,".");
+    }else if(item[i] == "11"){
+      item.splice(i,1,"-");
+    }else if(item[i] == "**"){
+      item.join('');
+   }else{
+     item.splice(i,1," ")
+   }
+  }
+}
+
+let newArrChange = newArr.map(e=>e.join('').trim());
+let newArrObj = Object.assign({}, newArrChange);
+let frase = '';
+
+  for(let elem of newArrChange){
+    if(elem=="**********"){
+      frase+=" ";
+    }
+    for(let key in MORSE_TABLE){
+     if(key == elem){
+          
+        frase+=MORSE_TABLE[elem];
+        
+    }
+  }
+  }
+  return console.log(frase);
+
 }
 
 module.exports = {
